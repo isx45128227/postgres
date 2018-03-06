8d9c246d8b4aa9cc7718ab88639be150d04da36b
isx45128227 <35956939+isx45128227@users.noreply.github.com>
2018-03-06 16:34:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,42 +1,14 @@
 #! /bin/bash
 # install.sh
-# @edt ASIX-M06 Curs 2017-2018
-#
-# Generar imatge postgres:base
+# @edt ASIX-M11 Curs 2017-2018
+# postgres:base
 # -----------------------------
 
-# mkdir /opt/docker
-
-# -------------------
-# Instal·lar softwae
-# -------------------
-# dnf -y update vi
-# dnf -y install vim postgresql postgresql-server postgresql-libs rpcbind passwd
-# dnf -y install findutils
-# dnf -y install nmap procps iputils iproute
-
-# mkdir /run/rpcbind 
-# touch /run/rpcbind/rpcbind.lock
-# /usr/sbin/rpcbind 
-
-# --------------------------------------
-# Configurar per permetre l'accés remo
-# --------------------------------------
-# vim /var/lib/pgsql/data/pg_hba.conf
-  # IPv4 local connections:
-#   host    all             all             127.0.0.1/32            trust
-#   host    all             all             172.17.0.2/32           trust
-#   host    all             all             0.0.0.0/0               trust
-
-# vim /var/lib/pgsql/data/postgresql.conf 
- # listen_addresses='*'
-
 # --------------------------
-# Configurar compte postgres 
+# Configurar compte 
 # --------------------------
-echo "jupiter" | passwd --stdin postgres
-useradd edtasixm06
-echo "edtasixm06" | passwd --stdin edtasixm06
+useradd roger
+echo "roger" | passwd --stdin roger
 
 # ----------------------------
 # Sessió com a usuari postgres
@@ -48,7 +20,8 @@ PG_OOM_ADJUST_FILE=/proc/self/oom_score_adj
 PG_OOM_ADJUST_VALUE=0
 PGSTARTTIMEOUT=270
 PGDATA=/var/lib/pgsql/data
-export OOMScoreAdjust PG_OOM_ADJUST_FILE PG_OOM_ADJUST_VALUE PGSTARTTIMEOUT PGDATA 
+export OOMScoreAdjust PG_OOM_ADJUST_FILE PG_OOM_ADJUST_VALUE PGSTARTTIMEOUT PGDATA
+
 # Crear l'estructura de dades /var/libpgsql/data i engegar el servei 
 pg_ctl initdb 
 /usr/bin/pg_ctl -D /var/lib/pgsql/data -l logfile start
@@ -58,23 +31,14 @@ pg_ctl initdb
 /usr/libexec/postgresql-ctl stop
 /usr/libexec/postgresql-ctl start
 
-# -----------------------------
-# Valors del postgresql.service
-# -----------------------------
-#/usr/libexec/postgresql-check-db-dir %N
-#/usr/libexec/postgresql-ctl start -D ${PGDATA} -s -w -t ${PGSTARTTIMEOUT}
-#stop   /usr/libexec/postgresql-ctl stop -D ${PGDATA} -s -m fast
-#reload /usr/libexec/postgresql-ctl reload -D ${PGDATA} -s
-
 # -----------------------------------------
-# Crear un compte d'usuari local edtasixm06
+# Crear un compte d'usuari local 
 # -----------------------------------------
 psql template1
-CREATE USER edtasixm06;
-#grant ALL ON DATABASE template1 TO edtasixm06;
-create database training;
-grant ALL ON DATABASE training to edtasixm06;
-grant ALL on oficinas, pedidos, productos, repventas, clientes to edtasixm06;
+CREATE USER roger;
+CREATE DATABASE training;
+GRANT ALL ON DATABASE training to roger;
+GRANT ALL ON oficinas, pedidos, productos, repventas, clientes to roger;
 
 \q
 psql training
@@ -89,17 +53,11 @@ psql training
 \copy repventas from /opt/docker/repventas.dat 
 \copy oficinas from /opt/docker/oficinas.dat  
 
+\q
 
-# --------------------------------------------
-# Crear la BD training com a usuari edtasixm06
-# --------------------------------------------
-exit
-su -l edtasixm01
-psql template1
-create database training;
 
 # ----------------
-# Exemples d'acces
+# Acces
 # ----------------
 # psql -qtA -F ';' training
 # psql -qtA -F ';' training -c "select * from oficinas";
