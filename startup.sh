abca22c20a3c4d74a7984bd8aabbcbb19e144099
isx45128227 <35956939+isx45128227@users.noreply.github.com>
2018-03-06 17:10:08 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# ----------------------------
-# Sessió com a usuari postgres
+# @edt ASIX-M11 Curs 2017-2018
+# postgres:base
 # ----------------------------
 # su -l postgres
 /usr/bin/pg_ctl -D /var/lib/pgsql/data -l logfile start
